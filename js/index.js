bba9db6c042a6d83aac8e564a8c0bea5b68822ca
MaryamYasser98 <maryamyyee98@gmail.com>
2021-12-08 16:21:51 +0200

    first commit

---
+++ js/index.js
@@ -0,0 +1,48 @@
+
+$(document).ready(function(){
+
+    $('#menu-bar').click(function(){
+        $(this).toggleClass('fa-times')
+        $('.navbar').toggleClass('nav_toggle')
+    });
+
+
+    $(window).on('scroll load' , function(){
+
+        $('#menu-bar').removeClass('fa-times')
+        $('.navbar').removeClass('nav_toggle')
+
+        $('section').each(function(){
+
+            let top = $(window).scrollTop();
+            let offset = $(this).offset().top - 200;
+            let height = $(this).height();
+            let id = $(this).attr('id');
+
+
+            if(top > offset && top < offset + height){
+                $('.navbar ul li a').removeClass('active')
+                $('.navbar').find(`[href="#${id}"]`).addClass('active')
+            }
+
+
+
+
+
+        }) // end of section funtion
+
+
+    })// end of window funtion
+
+
+    $('.menu .list .btn').click(function(){
+        $(this).addClass('active').siblings().removeClass('active')
+
+        let src = $(this).attr('data-src');
+        $('#menu-img').attr('src' , src)
+    })
+
+
+})// end of ready function
+
+
